f8a396012e41f741eecfa60fb5fe22f3aec1b2d9
Allan Henrique <allanhsz@outlook.com>
2021-10-01 00:04:16 -0300

    REFACTOR: Deletando Loader antigo

--- src/components/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Loader = () => (
-  <S.Wrapper>
-    <S.Loader />
-  </S.Wrapper>
-)
-
-export default Loader
--- src/components/Loader/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Loader from '.'
-
-export default {
-  title: 'Loader',
-  component: Loader
-} as Meta
-
-export const Default: Story = () => <Loader />
