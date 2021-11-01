5216ed0e571774621a9cff62f7f415ee97395870
Allan Henrique <allanhsz@outlook.com>
2021-10-31 23:46:24 -0300

    TEST: MyAccount ajustando testes

--- src/templates/MyAccount/test.tsx
+++ src/templates/MyAccount/test.tsx
@@ -1,13 +1,20 @@
-import { renderWithTheme } from 'utils/tests/helpers'
+import { render } from 'utils/test-utils'
 
 import MyAccount from '.'
 
+jest.mock('templates/Page', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Page">{children}</div>
+  }
+}))
+
 describe('<MyAccount />', () => {
   it('should render the heading', () => {
-    renderWithTheme(<MyAccount />)
+    const { container } = render(<MyAccount />)
 
     expect(true).toBeTruthy()
 
-    // expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
