14978d556a0e25e836f991e82149678dd0c7abb3
Allan Henrique <allanhsz@outlook.com>
2021-11-05 20:16:55 -0300

    FEATURE: Home integrando sessao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
+import { GetServerSidePropsContext } from 'next'
+import { getSession } from 'next-auth/client'
 import Home, { HomeProps } from 'templates/Home'
 
 export default function Index(props: HomeProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
   return {
     props: {
-      connections: 200
-    }
+      connections: 200,
+      username: session?.user?.name
+    } as HomeProps
   }
 }
