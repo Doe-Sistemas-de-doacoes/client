6780efc59f1a208f885c66e2a03f20560cfb6703
Allan Henrique <allanhsz@outlook.com>
2021-11-06 19:05:39 -0300

    FIX: Removendo stories dos template donation e find

--- src/templates/Donation/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Donation from '.'
-
-export default {
-  title: 'Donation',
-  component: Donation
-} as Meta
-
-export const Default: Story = () => <Donation />
