5ad3d2b980bc44f211b2bb0e49b476cae6198d89
Allan Henrique <allanhsz@outlook.com>
2021-11-06 19:04:17 -0300

    FIX: Toast ajustando stories

---
+++ src/components/Toast/mock.ts
@@ -0,0 +1,11 @@
+import { ToastProps } from '.'
+
+export const ToastSuccess: ToastProps = {
+  type: 'success',
+  message: 'A simple success message'
+}
+
+export const ToastError: ToastProps = {
+  type: 'error',
+  message: 'A simple error message'
+}
