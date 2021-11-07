48a69d5a5f32ccbe7eefa6e529008e1e5b997791
Allan Henrique <allanhsz@outlook.com>
2021-11-07 13:15:40 -0300

    FIX: Home ajustando serverSide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
-import { GetServerSidePropsContext } from 'next'
-import { getSession } from 'next-auth/client'
 import Home, { HomeProps } from 'templates/Home'
 
 export default function Index(props: HomeProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getSession(context)
-
+export async function getServerSideProps() {
   return {
     props: {
-      connections: 200,
-      username: session?.user?.name
+      connections: 200
     } as HomeProps
   }
 }
