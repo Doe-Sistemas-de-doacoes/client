3091ef55e0445baee89ce0a70d0bec53a666a2ba
Allan Henrique <allanhsz@outlook.com>
2021-11-13 21:26:48 -0300

    FEATURE: HandlerError tratando erros por codigo

--- src/utils/handle-error.ts
+++ src/utils/handle-error.ts
@@ -3,11 +3,26 @@ import { AxiosError } from 'axios'
 const handlerError = (error: string | AxiosError): string => {
   console.log('handlerError:', error)
 
-  if (typeof error === 'string') return error
+  if (error) {
+    if (typeof error === 'string') return error
 
-  if (error?.response?.data?.error) return error?.response?.data?.error
+    if (error?.response?.data?.error) return error?.response?.data?.error
 
-  if (typeof error?.response?.data === 'string') return error?.response?.data
+    if (typeof error?.response?.data === 'string') return error?.response?.data
+
+    if (error?.response?.status) {
+      switch (error?.response?.status) {
+        case 503:
+          return 'Estamos enfrentando problemas em nesso servidor, tente novamente em breve'
+        case 401:
+          return 'Você não está conectado, entre em uma conta para prosseguir'
+        case 403:
+          return 'Você não possuir acesso a essa opção'
+        case 504:
+          return 'Não foi possivel se comunicar, verifique sua conexão'
+      }
+    }
+  }
 
   return 'Erro desconhecido'
 }
