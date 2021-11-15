380ca09663bdf9e53954c197e1caaa7b3dd386b9
Allan Henrique <allanhsz@outlook.com>
2021-11-15 14:04:13 -0300

    FEATURE: Criando template Profile

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+
+import { ErrorProps } from 'components/Error'
+import ProfileMenu from 'components/ProfileMenu'
+import Page from 'templates/Page'
+
+import * as S from './styles'
+
+export type ProfileTemplateProps = {
+  children: React.ReactNode
+  error?: ErrorProps
+}
+
+const Profile = ({ children, error }: ProfileTemplateProps) => {
+  const { asPath } = useRouter()
+
+  return (
+    <Page title="Minha conta" footer header decoration="corners" error={error}>
+      <S.Main>
+        <ProfileMenu activeLink={asPath} />
+        <S.Content>{children}</S.Content>
+      </S.Main>
+    </Page>
+  )
+}
+
+export default Profile
---
+++ src/templates/Profile/styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Main = styled.main`
+  ${({ theme }) => css`
+    width: 100%;
+
+    @media screen and (min-width: 960px) {
+      display: grid;
+      grid-template-columns: 32rem 1fr;
+      gap: ${theme.spacings.small};
+    }
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: ${theme.colors.white};
+    box-shadow: ${theme.boxShadow.medium};
+    padding: ${theme.spacings.xsmall};
+    color: ${theme.colors.text};
+
+    ${media.greaterThan('small')`
+      padding: ${theme.spacings.small};
+    `}
+  `}
+`
