4c872552500649676214f26d1eb87029cc822da2
Allan Henrique <allanhsz@outlook.com>
2021-11-19 00:12:54 -0300

    FEATURE: Home conectando com a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
+import { AxiosError } from 'axios'
+import { GetServerSidePropsContext } from 'next'
+
+import { apiSSR } from 'services/api'
 import Home, { HomeProps } from 'templates/Home'
+import handlerError from 'utils/handle-error'
 
 export default function Index(props: HomeProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      connections: 200
-    } as HomeProps
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  try {
+    const response = await apiSSR(context).get<BigInt>('/donations/count')
+
+    return {
+      props: {
+        connections: response.data
+      }
+    }
+  } catch (error) {
+    return {
+      props: {
+        error: {
+          message: handlerError(error as AxiosError)
+        }
+      }
+    }
   }
 }
