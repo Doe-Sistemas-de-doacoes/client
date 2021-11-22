c71a5b261acd47273b50ea1ecc21df10e41d041a
Allan Henrique <allanhsz@outlook.com>
2021-11-21 22:57:56 -0300

    FEATURE: Criando componente DonationItem

--- src/components/DonationItem/index.tsx
+++ src/components/DonationItem/index.tsx
@@ -1,21 +1,56 @@
-import { UserProps } from 'services/user'
+import HeadingSection from 'components/HeadingSection'
+import { UserProps, AddressProps } from 'services/user'
+import formatDate from 'utils/format-date'
+
 import * as S from './styles'
 
+type User = {
+  address: AddressProps
+} & Pick<UserProps, 'id' | 'name'>
+
 export type DonationItemProps = {
-  datetimeOfCollection: string
-  datetimeOfDelivery: string
-  description: string
-  donor: UserProps
+  date: string
   id: number
-  receiver: UserProps
-  typeOfDonation: string
+  description: string
+  donor: User
+  receiver: User
+  isDelivery: boolean
+  status: 'FINALIZADO' | 'PENDENTE'
+  type: string
 }
 
-const DonationItem = ({ description, typeOfDonation }: DonationItemProps) => (
+const DonationItem = ({
+  description,
+  date,
+  donor,
+  type
+}: DonationItemProps) => (
   <S.Wrapper>
-    <S.Img />
-    <S.Type>{typeOfDonation}</S.Type>
-    <S.Description>{description}</S.Description>
+    <S.Image />
+
+    <S.Content>
+      <S.Section>
+        <S.Type>{type}</S.Type>
+        <S.Date>{formatDate(date)}</S.Date>
+
+        <S.Description>{description}</S.Description>
+      </S.Section>
+
+      {!!donor?.address && (
+        <S.Addresses>
+          <HeadingSection>Endereço</HeadingSection>
+          <S.Address>
+            <p>
+              {donor.address?.street}, {donor.address?.number} -{' '}
+              {donor.address?.neighborhood}
+            </p>
+            <p>
+              {donor.address?.city} / {donor.address?.state}
+            </p>
+          </S.Address>
+        </S.Addresses>
+      )}
+    </S.Content>
   </S.Wrapper>
 )
 
--- src/components/DonationItem/styles.ts
+++ src/components/DonationItem/styles.ts
@@ -1,35 +1,91 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    background: #fff;
-    gap: ${theme.spacings.xsmall};
-    border-radius: ${theme.border.radius.medium};
-    padding: ${theme.spacings.xsmall};
+    background: ${theme.colors.extraLightGray};
+    border-radius: ${theme.border.radius.small};
     box-shadow: ${theme.boxShadow.medium};
   `}
 `
 
-export const Img = styled.div`
+export const Image = styled.div`
   ${({ theme }) => css`
     width: 100%;
     height: 24rem;
-    padding: ${theme.spacings.xsmall};
-    border-radius: ${theme.border.radius.medium};
+    grid-area: image;
+    border-radius: ${theme.border.radius.small};
     background: ${theme.colors.lightGray};
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    padding: ${theme.spacings.small};
+    gap: ${theme.spacings.small};
+
+    ${media.greaterThan('small')`
+      padding: ${theme.spacings.medium};
+      gap: ${theme.spacings.medium};
+    `}
+  `}
+`
+
+export const Section = styled.section`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-areas:
+      'type date'
+      'description description';
+    gap: ${theme.spacings.xsmall};
   `}
 `
 
 export const Type = styled.h5`
   ${({ theme }) => css`
+    grid-area: type;
     color: ${theme.colors.textEmphasis};
+  `}
+`
+
+export const Date = styled.span`
+  ${({ theme }) => css`
+    grid-area: date;
+    color: ${theme.colors.text};
+    font-size: ${theme.font.sizes.xsmall};
+    color: ${theme.colors.gray};
+    text-align: right;
+    text-transform: uppercase;
   `}
 `
 
 export const Description = styled.p`
   ${({ theme }) => css`
+    grid-area: description;
+    color: ${theme.colors.gray};
+    font-size: ${theme.font.sizes.small};
+  `}
+`
+
+export const Addresses = styled.section`
+  ${({ theme }) => css`
+    display: flex;
+    grid-area: address;
+    gap: ${theme.spacings.xsmall};
+    flex-direction: column;
+  `}
+`
+
+export const Address = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacings.xxsmall};
     color: ${theme.colors.text};
+    font-size: ${theme.font.sizes.small};
   `}
 `
