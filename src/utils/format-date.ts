5d4e324874ef1b136cd63233ec2d2d4db5f13958
Allan Henrique <allanhsz@outlook.com>
2021-11-24 15:33:45 -0300

    FIX: FormatDate ajustando metodo

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,7 +1,7 @@
 function formatDate(date: string): string {
-  if (date) return ''
+  if (!date) return ''
 
-  if (date.length > 10) return date.split('-').reverse().join('/')
+  if (date.length > 10) return date.slice(0, 10).split('-').reverse().join('/')
 
   return ''
 }
