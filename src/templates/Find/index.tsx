3b876cdcb61d9d519f5c3b8554f1f4a039de5958
Allan Henrique <allanhsz@outlook.com>
2021-11-24 15:37:35 -0300

    FIX: Find migrando tipos

--- src/templates/Find/index.tsx
+++ src/templates/Find/index.tsx
@@ -1,16 +1,17 @@
 import DonationList, { DonationListProps } from 'components/DonationList'
 import { ErrorProps } from 'components/Error'
 import Page from 'templates/Page'
+
 import * as S from './styles'
 
 export type FindTemplateProps = {
   error?: ErrorProps
 } & DonationListProps
 
-const FindTemplate = ({ donations }: DonationListProps) => (
+const FindTemplate = ({ items }: DonationListProps) => (
   <Page title="Encontrar" header footer decoration="topRightCorner">
     <S.Wrapper>
-      <DonationList donations={donations} />
+      <DonationList items={items} />
     </S.Wrapper>
   </Page>
 )
